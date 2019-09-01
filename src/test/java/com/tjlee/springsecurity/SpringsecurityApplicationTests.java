9552ee23f6a40e39fd18674324303db2948249ca
LEETAEJIN <tmffjtl21@osstem.com>
2019-09-02 01:44:05 +0900

    11

--- src/test/java/com/tjlee/springsecurity/SpringsecurityApplicationTests.java
+++ src/test/java/com/tjlee/springsecurity/SpringsecurityApplicationTests.java
@@ -9,19 +9,29 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)        // 실제 디비를 사용하는 설정 (조심)
+// 실제 디비를 사용하는 설정, test는 기본이 롤백, 롤백을 원하지 않을 경우 @Rollback(false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpringsecurityApplicationTests {
 
     @Autowired
     AccountRepository accountRepository;
 
     @Test
+//    @Rollback(false)
     public void crud() {
-        Account account = accountRepository.findByUsername("tjlee");
-        assertThat(account).isNull();
+
+        Account account = new Account();
+        account.setUsername("tjlee1");
+        account.setPassword("1111");
+        accountRepository.save(account);
+
+        Account account1 = accountRepository.findByUsername("tjlee1");
+        assertThat(Optional.ofNullable(account1).map(Account::getUsername).orElse(null)).isEqualTo("tjlee1");
     }
 }
